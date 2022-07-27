7c3cb1597cef7fc161aa69567db1dc4564988053
Jose Flores <jfloresxc@gmail.com>
2022-07-27 11:16:03 -0500

    “Parte 3 - Registrar Estudiantes - OK”

--- AFM_UC04/urls.py
+++ AFM_UC04/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludo/', views.saludo, name = "saludo"),
     path('integrantes/', views.integrantes, name = "integrantes"),
+    path('crearestudiante/', views.crear_estudiante, name = "crearestudiante"),
 ]
